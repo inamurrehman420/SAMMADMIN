4b87dc9cf9180ab438c8283098d9f5e5419b7c60
inam.rehman <inam.rehman@aksa-sds.com>
2023-11-10 17:51:42 +0500

    design work

--- documentation/js/material-dashboard.js
+++ documentation/js/material-dashboard.js
@@ -4,8 +4,8 @@
  * Material Dashboard Angular 2 - V1.2.0
  =========================================================
 
- * Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
- * Copyright 2017 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.SAMM.com/product/material-dashboard-angular2
+ * Copyright 2017 SAMM (https://www.SAMM.com)
  * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular/blob/master/LICENSE.md)
 
  =========================================================
--- src/assets/css/demo.css
+++ src/assets/css/demo.css
@@ -4,11 +4,11 @@
 * Material Dashboard Angular - v2.8.0
 =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Product Page: https://www.SAMM.com/product/material-dashboard-angular2
+* Copyright 2021 SAMM (https://www.SAMM.com)
 * Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular2/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by SAMM
 
 =========================================================
 
