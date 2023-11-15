f02e83613f77605206a32ccf23a833cd14c9c639
unknown <abdulsamirana27@gmail.com>
2023-11-15 17:24:47 +0500

    user add screen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7001/api/',
-   imageBaseUrl: 'http://localhost:7001',
-  // apiUrl: 'https://e668-154-192-18-80.ngrok-free.app/api/', //ngrok
-  // imageBaseUrl: "https://e668-154-192-18-80.ngrok-free.app"
+  // apiUrl: 'http://localhost:7001/api/',
+  //  imageBaseUrl: 'http://localhost:7001',
+  apiUrl: 'https://84ae-154-192-18-80.ngrok.io/api/', //ngrok
+  imageBaseUrl: "https://84ae-154-192-18-80.ngrok.io"
 };
 
