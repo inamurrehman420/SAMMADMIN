57f776444e8be846e8e6aa07df1ca1229dc84245
Umar551 <umar.naeem551@gmail.com>
2024-01-28 20:02:18 +0500

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:7001/api/',
-  imageBaseUrl: 'http://localhost:7001',
-  // apiUrl: 'http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com/api/', //ngrok
-  // imageBaseUrl: "http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com"
+  apiUrl: 'http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com/api/', //ngrok
+  imageBaseUrl: "http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com"
 };
